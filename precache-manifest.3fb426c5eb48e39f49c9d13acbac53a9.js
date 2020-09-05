18b98ae10ecefff645a40c7f955f5f198ad81b10
davisiiala <davisiiala@gmail.com>
2020-09-04 21:10:23 -0400

    Updates

---
+++ precache-manifest.3fb426c5eb48e39f49c9d13acbac53a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd439f2ba288e737b90de39886e3af5",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "d91b9f7f79ace773ba8c",
+    "url": "/mysite/static/css/2.c6f1d12b.chunk.css"
+  },
+  {
+    "revision": "35430df72f31346c1ad0",
+    "url": "/mysite/static/css/main.13fd1882.chunk.css"
+  },
+  {
+    "revision": "d91b9f7f79ace773ba8c",
+    "url": "/mysite/static/js/2.7efc3d0b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mysite/static/js/2.7efc3d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35430df72f31346c1ad0",
+    "url": "/mysite/static/js/main.217989ed.chunk.js"
+  },
+  {
+    "revision": "7304cfa601128363c722",
+    "url": "/mysite/static/js/runtime-main.3c028e1a.js"
+  }
+]);
